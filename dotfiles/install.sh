85c77b31c29bf3bbe8728dd2725d8b7a8a8daac4
Fredrik Olsson <spooky.bender@gmail.com>
2016-11-17 00:27:37 +0100

    logging for install script

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -3,6 +3,22 @@ DOTFILES_HOME=~/dotfiles
 DOTFILES_LIB=$DOTFILES_HOME/lib
 FEATURE_HOME=$DOTFILES_HOME/features
 
+function start_feature(){
+  echo ""
+  echo ""
+  echo "START $1"
+  echo "------------------------------------"
+  echo "------------------------------------"
+}
+
+function stop_feature(){
+  echo ""
+  echo ""
+  echo "STOP $1"
+  echo "------------------------------------"
+  echo "------------------------------------"
+}
+
 function install_cron(){
   CRON_FILE=/tmp/mycron
   #write out current crontab
@@ -15,36 +31,46 @@ function install_cron(){
 }
 
 # submodules
+start_feature "submodules"
 git submodule update --init --recursive
+stop_feature "submodules"
 
 # apps
+start_feature "apt-get install"
 sudo apt update
 sudo apt install tmux htop
 sudo apt-get install python3.5
+stop_feature "apt-get install"
 
 # update
+start_feature "update pip"
 sudo -H python3.5 `which pip` install --upgrade pip
+stop_feature "update pip"
 
 # vdirsyncer
 VDIR_FEATURE=$FEATURE_HOME/vdirsyncer
 if [ ! -e "$VDIR_FEATURE" ]; then
-## otath support for googlecalendar
-sudo -H python3.5 `which pip` install requests requests_oauthlib
-
-## install
-sudo -H python3.5 `which pip` install git+https://github.com/untitaker/vdirsyncer.git
-
-## crontab
-install_cron "*/5 * * * *	$DOTFILES_LIB/vdirsyncer_cron.sh"
-
-touch $VDIR_FEATURE
+  start_feature "vdirsyncer" 
+  ## otath support for googlecalendar
+  sudo -H python3.5 `which pip` install requests requests_oauthlib
+  
+  ## install
+  sudo -H python3.5 `which pip` install git+https://github.com/untitaker/vdirsyncer.git
+  
+  ## crontab
+  install_cron "*/5 * * * *	$DOTFILES_LIB/vdirsyncer_cron.sh"
+  
+  touch $VDIR_FEATURE
+  stop_feature "vdirsyncer" 
 fi
 
 # khal
 KHAL_HOME=$FEATURE_HOME/khal
 if [ ! -e $KHAL_HOME ]; then
-## 
-sudo -H python3.5 `which pip` install git+https://github.com/pimutils/khal
-
-touch $KHAL_HOME
+  start_feature "khal"
+  ## 
+  sudo -H python3.5 `which pip` install git+https://github.com/pimutils/khal
+  
+  touch $KHAL_HOME
+  stop_feature "khal"
 fi
