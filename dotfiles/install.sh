04723fb5fc6ea84713275a95081b7737dd974b99
Fredrik Olsson <spooky.bender@gmail.com>
2016-11-17 21:08:36 +0100

    davmail

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 DOTFILES_HOME=~/dotfiles
 DOTFILES_LIB=$DOTFILES_HOME/lib
+USER_BIN=~/bin
+USER=`whoami`
+GROUP=$USER
 FEATURE_HOME=$DOTFILES_HOME/features
+
+function echoerr() { echo "$@" 1>&2; }
+
+function failed_feature(){
+  echoerr "FAILED"
+  echoerr "FAILED"
+  echoerr "FAILED $1"
+  echoerr "------------------------------------"
+  echoerr "------------------------------------"
+}
 
 function start_feature(){
   echo ""
@@ -30,6 +43,13 @@ function install_cron(){
   rm $CRON_FILE
 }
 
+sudo echo "start" || exit 1
+
+# setup directory structure
+if [ ! -e $USER_BIN ]; then
+  mkdir $USER_BIN || exit 1
+fi
+
 # submodules
 start_feature "submodules"
 git submodule update --init --recursive
@@ -37,9 +57,17 @@ stop_feature "submodules"
 
 # apps
 start_feature "apt-get install"
+
 sudo apt update
-sudo apt install tmux htop
-sudo apt-get install python3.5
+sudo apt      -y  install tmux htop
+sudo apt-get  -y  install python3.5
+sudo apt-get  -y  install w3m
+sudo apt-get  -y  install feh
+sudo apt-get  -y  install antiword
+sudo apt-get  -y  install catdoc
+sudo apt-get  -y  install python-sqlite
+sudo apt-get  -y  install python-vobject
+
 stop_feature "apt-get install"
 
 # update
@@ -77,9 +105,65 @@ fi
 
 #davmail
 DAVMAIL_FEATURE=$FEATURE_HOME/davmail
-if [ ! -e $DAVMAIL_HOME ];then
+if [ ! -e $DAVMAIL_FEATURE ];then
   start_feature "davmail"
-# wget http://sourceforge.net/projects/davmail/files/davmail/4.7.2/davmail_4.7.2-2427-1_all.deb
-  touch $DAVMAIL_FEATURE
-  stop_feature "davmail"
+  
+  DAVMAIL_ZIP=$USER_BIN/davmail.zip
+  wget -O $DAVMAIL_ZIP https://sourceforge.net/projects/davmail/files/latest/download?source=files
+  RET=$?
+
+  if [ $RET -eq 0 ];then
+    unzip $DAVMAIL_ZIP -d $USER_BIN
+    rm $DAVMAIL_ZIP
+  
+    # wget http://sourceforge.net/projects/davmail/files/davmail/4.7.2/davmail_4.7.2-2427-1_all.deb
+
+    DAVMAIL_SERVICE_SRC=~/dotfiles/service/davmail
+    DAVMAIL_SERVICE_DEST=/etc/init.d/davmail
+    if [ ! -e $DAVMAIL_SERVICE_DEST ];then
+      sudo cp $DAVMAIL_SERVICE_SRC /etc/init.d
+      sudo chmod a+x $DAVMAIL_SERVICE_DEST
+      # TODO fix service
+      sudo service davmail start
+      sudo update-rc.d davmail defaults
+
+    fi
+
+    touch $DAVMAIL_FEATURE
+    stop_feature "davmail"
+  else
+    failed_feature "davmail remote zip"
+  fi
 fi
+
+#lbdb
+LBDB_FEATURE=$FEATURE_HOME/lbdb
+if [ ! -e $LBDB_FEATURE ]; then
+  start_feature "LBDB"
+	sudo apt-get -y install lbdb
+  RET=$?
+  if [ $RET -ne 0 ]; then
+    failed_feature "LBDB"
+  else
+    stop_feature "LBDB"
+  fi
+  touch $LBDB_FEATURE
+fi
+
+#offlineimap
+OFFLINEIMAP_FEATURE=$FEATURE_HOME/offlineimap
+if [ ! -e $OFFLINEIMAP_FEATURE ]; then
+  start_feature "offlineimap"
+  # TODO fix
+  install_cron "*/5 * * * *	~/dotfiles/lib/offlineimap_cron.sh"
+  stop_feature "offlineimap"
+fi
+
+#mutt
+MUTT_FEATURE=$FEATURE_HOME/mutt
+if [ ! -e $MUTT_FEATURE ]; then
+  start_feature "mutt"
+  # TODO fix
+  stop_feature "mutt"
+fi
+
