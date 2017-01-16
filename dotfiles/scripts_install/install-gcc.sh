c11c767390249ac926a876ac7bdd178b09d0f6c5
Fredrik Olsson <spooky.bender@gmail.com>
2017-01-16 18:08:26 +0100

    install gcc

--- dotfiles/scripts_install/install-gcc.sh
+++ dotfiles/scripts_install/install-gcc.sh
@@ -5,26 +5,32 @@ exit 1
 
 PREV_DIR=`pwd`
 
-TEMP_DIR=`mktemp -d`
-cd $TEMP_DIR
+SOURCE_DIR="~/sources"
+mkdir -p $SOURCE_DIR
+
+cd $SOURCE_DIR
 
 GCC_UNTAR=gcc-6.2.0
 GCC_NAME=$GCC_UNTAR
+if [ -e $GCC_UNTAR ]; then
+  rm -rf $GCC_UNTAR
+fi
 GCC_TAR=$GCC_UNTAR.tar.bz2
 wget ftp://ftp.nluug.nl/mirror/languages/gcc/releases/$GCC_NAME/$GCC_TAR
 
-tar -xvf $GCC_TAR
+tar -xf $GCC_TAR || exit 1
 rm $GCC_TAR
 
 cd $GCC_UNTAR
 
 # precrec
-./contrib/download_prerequisites 
+./contrib/download_prerequisites  || exit 1
 # configure installation
-./configure --prefix=/usr/ --disable-multilib
-# build
-make
+./configure --prefix=/usr/local --disable-multilib || exit 1
+# build using pipeline
+# make -pipe
+make -j 6 > /dev/null || exit 1
 # install
-sudo make install
+sudo make install || exit 1
 
 cd $PREV_DIR
