6737050eea62e9b7329d54aeca8cdccfa9ca8559
Fredrik Olsson <spooky.bender@gmail.com>
2017-01-19 09:37:54 +0100

    add missing files

---
+++ dotfiles/lib/git_root.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# set -e
+path="`pwd`"
+# shift 1
+# echo "|$path"
+while [[ "$path" != "/" ]];
+do
+    GIT_PATH="$path/.git" 
+    ls $GIT_PATH > /dev/null 2>&1
+    RET=$?
+    if [ $RET -eq 0 ];then
+      echo "$path"
+      exit 0
+    fi
+    # echo "$GIT_PATH/$RET"
+    #  make relative path absolute
+    path="$(readlink -f $path/..)"
+done
+echo "`pwd`"
