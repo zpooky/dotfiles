1ab072200bb95ba2f8ee80f66df3c05174b93c93
Fredrik Olsson <spooky.bender@gmail.com>
2017-09-16 20:33:14 +0200

    nfs

--- arch.sh
+++ arch.sh
@@ -137,3 +137,6 @@ passwd spooky
 groupadd sudo
 usermod-a -G sudo spooky
 visudo -f /etc/sudoers # uncomment sudo group access
+
+#nfs
+sudo mount 192.168.1.12:/i-data/0da29454/nfs/video /home/spooky/mount/video
