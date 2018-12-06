8b68c932188f5b2713f0f3c5bfb3e526fc06ecbe
Fredrik Olsson <spooky.bender@gmail.com>
2018-12-06 16:29:26 +0100

    git submodule jobs
    Change-Id: Icedfce4c919af50ad8c940963b8d1c001a1431d9

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -34,6 +34,7 @@ fi
 start_feature "git submodules"
 # git submodule sync --recursive || exit 1
 # recursivly pull in all submodule repos
+echo "git submodule update --init --recursive --remote --jobs 8"
 git submodule update --init --recursive --remote --jobs 8 || exit 1
 # git submodule update --init --recursive || exit 1
 stop_feature "git submodules"
