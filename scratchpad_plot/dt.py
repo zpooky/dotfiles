13e9bcc586fbe8ceef2ba0424f45a880482b7894
fred <spooky.bender@gmail.com>
2021-07-23 22:09:13 +0000

    imp

--- scratchpad_plot/dt.py
+++ scratchpad_plot/dt.py
@@ -24,24 +24,49 @@ def find_period(ω):
     T = T + 1
   return int(T * N)
 
+def realDFT(f, x):
+  """
+  DFT:
+          N-1
+    X[k] = ∑ 𝑥[n]e⁻ⁱ²πᵏⁿ/ᴺ
+          n=0
+
+  Euler formula:
+    e⁻ⁱᵠ = cos(φ) -i⋅sin(φ) = cos(-φ) + i⋅sin(-φ)
+  """
+  N = len(x)
+  result = 0
+  for n in range(0, N):
+    arg = 2.*π*k*(n/N)
+    result += x[n] * np.complex(np.cos(arg), -np.sin(arg))
+  return result
 
 start = 0
 
-# ω = np.pi*(1./8.)
-# ω = np.pi
-# ω = (4*np.pi)/5
-# ω = π / 4
-ω = np.pi/4
-period = find_period(ω)
-print("period: {}".format(period))
-if period < 200:
-  n = np.arange(start, start + period)
-else:
-  stop = 8
-  n = np.arange(start, stop)
-x = np.cos(ω * n)
+def plot_signal():
+  # ω = np.pi*(1./8.)
+  # ω = np.pi
+  # ω = (4*np.pi)/5
+  # ω = π / 4
+  ω = np.pi/4
+  period = find_period(ω)
+  print("period: {}".format(period))
+  if period < 200:
+    n = np.arange(start, start + period)
+  else:
+    stop = 8
+    n = np.arange(start, stop)
+  x = np.cos(ω * n)
 
-plt.xlabel('n')
-plt.ylabel('x[n]')
-plt.stem(n, x)
-plt.show()
+  plt.xlabel('n')
+  plt.ylabel('x[n]')
+  plt.stem(n, x)
+  plt.show()
+
+def plot_DFT():
+  n = np.arange(start, stop)
+  ω = 20*π
+  x = 2*np.cos(ω*n+π/4)
+
+plot_DFT()
+realDFT()
