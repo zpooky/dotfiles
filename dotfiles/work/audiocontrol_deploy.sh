eef3918cba04f91a7ade1b25147e1ffd985945f5
Fredrik <spooky.bender@gmail.com>
2021-09-01 17:23:10 +0200

    imp

--- dotfiles/work/audiocontrol_deploy.sh
+++ dotfiles/work/audiocontrol_deploy.sh
@@ -4,10 +4,14 @@ l_pass="pass"
 l_ip="192.168.0.90"
 
 tmp="$(mktemp -d)"
+dir="/home/fredriol/dists/chanel34"
+product="q3536-lve"
+# dir="/home/fredriol/dists/coco-1-mic_disable"
+# product="q1656"
 if [ $? -eq 0 ]; then
   cd "${tmp}" || exit 1
 
-  scp fredriol@pc32557-1841:/home/fredriol/dists/chanel16-audio2/meta-axis-bsp/conf/machine/product-config/audiocontrol-conf/q3536-lve/devices.json .
+  scp fredriol@pc32557-1841:${dir}/meta-axis-bsp/conf/machine/product-config/audiocontrol-conf/${product}/devices.json .
   if [ $? -eq 0 ]; then
     sshpass -p "${l_pass}" scp devices.json root@${l_ip}:/usr/share/audiocontrol
     if [ ! $? -eq 0 ]; then
@@ -17,7 +21,7 @@ if [ $? -eq 0 ]; then
     echo "copy devices.json from dev failed"
   fi
 
-  scp fredriol@pc32557-1841:/home/fredriol/dists/chanel16-audio2/builds/q3536-lve/workspace/sources/audiocontrol/oe-workdir/package/usr/bin/audiocontrol .
+  scp fredriol@pc32557-1841:${dir}/builds/${product}/workspace/sources/audiocontrol/oe-workdir/package/usr/bin/audiocontrol .
   if [ $? -eq 0 ]; then
     sshpass -p "${l_pass}" ssh -qt root@${l_ip} "systemctl stop audiocontrol"
     if [ ! $? -eq 0 ]; then
--- dotfiles/work/monolith_deploy.sh
+++ dotfiles/work/monolith_deploy.sh
@@ -4,19 +4,23 @@ l_pass="pass"
 l_ip="192.168.0.90"
 
 tmp="$(mktemp -d)"
+dir="/home/fredriol/chanel31-latency"
 if [ $? -eq 0 ]; then
   cd "${tmp}" || exit 1
 
-  scp fredriol@pc32557-1841:/home/fredriol/dists/chanel16-audio2/builds/q3536-lve/workspace/sources/monolith/monolith/oe-workdir/package/usr/bin/monolith .
+  scp fredriol@pc32557-1841:${dir}/builds/q3536-lve/workspace/sources/monolith/monolith/oe-workdir/package/usr/bin/monolith .
   if [ $? -eq 0 ]; then
     sshpass -p "${l_pass}" ssh -qt root@${l_ip} "systemctl stop monolith"
     if [ ! $? -eq 0 ]; then
       echo " stop MONOLITH failed"
     fi
     ls -alh monolith
+    sha256sum monolith
     sshpass -p "${l_pass}" scp monolith root@${l_ip}:/usr/bin
     if [ ! $? -eq 0 ]; then
       echo " copy MONOLITH failed"
+    else
+      sshpass -p "${l_pass}" ssh -qt root@${l_ip} "sha256sum /usr/bin/monolith"
     fi
     sshpass -p "${l_pass}" ssh -qt root@${l_ip} "systemctl start monolith"
     if [ ! $? -eq 0 ]; then
