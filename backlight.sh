529c285eab39678511acfdb7d509996811528fa9
Fredrik Olsson <spooky.bender@gmail.com>
2024-04-02 10:39:24 +0200

    imp
    Change-Id: Icefc0d3cede7111709fdab148632f321d74df2ab

--- backlight.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo chmod 777 /sys/class/backlight/nv_backlight/brightness
-#sudo echo 5 > /sys/class/backlight/nv_backlight/brightness
-sudo echo 0 > /sys/class/backlight/nv_backlight/brightness
